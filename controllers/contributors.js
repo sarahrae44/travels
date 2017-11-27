c251bc25daa020b46b5d8b2065012f554b87a841
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-11-27 07:37:06 -0700

    added contributors show

--- controllers/contributors.js
+++ controllers/contributors.js
@@ -1,12 +1,31 @@
 const express = require('express');
 const router = express.Router();
+const Contributor = require('../models/contributors.js');
 
 router.get('/', (req, res) => {
-  res.render('contributors/index.ejs');
+  Contributor.find({}, (err, foundContributors) => {
+    res.render('contributors/index.ejs', {
+      contributors: foundContributors
+    });
+  })
 });
 
 router.get('/new', (req, res) => {
   res.render('contributors/new.ejs');
+});
+
+router.post('/', (req, res) => {
+  Contributor.create(req.body, (err, createdContributor) => {
+    res.redirect('/contributors');
+  });
+});
+
+router.get('/:id', (req, res) => {
+  Contributor.findById(req.params.id, (err, foundContributor) => {
+    res.render('contributors/show.ejs', {
+      contributor: foundContributor
+    });
+  });
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 const contributorsController = require('./controllers/contributors.js');
 
+app.use(bodyParser.urlencoded({extended:false}));
 app.use('/contributors', contributorsController);
 
 app.get('/', (req, res) => {
