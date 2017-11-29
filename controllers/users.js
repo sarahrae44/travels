22d5326680c7428864857930b28896a8f22f1a9f
Sarah Anderson <sarahanderson@MacBook-Pro.local>
2017-11-29 08:25:20 -0700

    able to add new user

--- controllers/users.js
+++ controllers/users.js
@@ -1,8 +1,15 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/users.js');
 
 router.get('/new', (req, res) => {
   res.render('users/new.ejs');
+});
+
+router.post('/', (req, res) => {
+  User.create(req.body, function(err, createdUser){
+    res.redirect('/');
+  });
 });
 
 module.exports = router;
